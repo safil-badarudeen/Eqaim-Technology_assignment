d9f9b86ae86316555f5360bc7627f73821055175
safil badarudeen <safilbadarudeen@gmail.com>
2023-02-10 23:14:08 +0530

    backend - added not found middleware - added morgan package to know which route we are hitting while testing in console - added express json to parse data from body

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,43 @@
+//.env , async-error-handler
 require("dotenv").config();
 require("express-async-errors");
 
+//express
 const express = require('express');
 const app = express();
 const port = 3000;
 
 
-//database
+//database(mongoDB)
 const {dbConnection} = require('./db/db')
+
+//router
+
+
+//other
+const morgan = require('morgan')
+
+const notFoundMiddleware = require('./middleware/not-found')
+
+
+//bodyParse middleware
+app.use(express.json())
+
+app.use(morgan('tiny'))
+
+
+
 app.get('/',(req,res)=>{
     res.send("project is on")
 })
+
+
+app.use(notFoundMiddleware)
+
+
+
+
+
 
 
 const start = async () => {
